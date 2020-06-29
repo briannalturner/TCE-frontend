722bc1be5f770fd8bdf21401f1ec7a4bcb967eef
briannalturner <blturner3527@gmail.com>
2020-06-29 19:06:32 -0400

    adding css to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import SmallBusinessLogo from '../pictures/sba.png'
+import {Link} from 'react-router-dom'
 
 class Footer extends React.Component {
 
     render() {
         return (
-            <footer class='bg-dark white-text' style={{width: "100%"}}>
+            <footer class='footer white-text p-2' style={{width: "100%"}}>
                 <div class='row'>
                     <div class='col'>
-                        <p class='navbar-brand'>info info info</p>
+                        <img src={SmallBusinessLogo} className="" style={{maxHeight: "100px"}} alt="small business logo" />
+                    </div>
+                    <div class='col'>
+                        <p class=''><small>At Turner Concrete & Excavating we strive to deliver a quality product each and every day. We have provided our services throughout the Shenandoah Valley and we are proud to be family owned and operated.</small></p>
+                    </div>
+                    <div class='col p-2'>
+                        <p class='d-block'><small><Link activeClassName="active" className="footer-text" to='/about'>About Us</Link></small></p>
+                        <p class='d-block'><small><Link activeClassName="active" className="footer-text" to='/careers'>Careers</Link></small></p>
+                        <p class='d-block'><small><Link activeClassName="active" className="footer-text" to='/photos'>Photo Gallery</Link></small></p>
+                        <p class='d-block'><small><Link activeClassName="active" className="footer-text" to='/contact'>Contact Us</Link></small></p>
                     </div>
                 </div>
             </footer>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,25 +9,25 @@ class HomePage extends React.Component {
                 <div className='jumbotron jumbotron-fluid px-5'>
                     <h1 className='display-2 text-left'>Let Us Make Your Dreams Concrete.</h1>
                     <br></br>
+                    <p className='lead text-left'>Call us at (540)742-3180 for a free consultation.</p>
                     <p className="text-left">At Turner Concrete & Excavating we strive to deliver a quality product each and every day, and our team will do their utmost to ensure your satisfaction. We have provided our services throughout the Shenandoah Valley and we are proud to be family owned and operated.</p>
                     <br></br>
-                    <p className='lead text-left'>Call us at (540)742-3180 for a free consultation.</p>
                     <Link to="/photos" className='btn btn-lg btn-primary float-left'>View Our Past Work</Link>
                 </div>
                 <div class="row mx-3 mb-4">
-                    <div class="col-md card-grow rounded m-1 py-2 blue-background clickable" onClick={() => window.location = "/about"}>
+                    <div class="col-md card-grow rounded m-1 py-2 blue-background white-text clickable" onClick={() => window.location = "/about"}>
                         <h3 class="display-4">The Turner Concrete Philosophy</h3>
                         <p>
                         Get to know us as a company.
                         </p>
                     </div>
-                    <div class="col-md card-grow rounded m-1 py-2 blue-background clickable" onClick={() => window.location = "/reviews"}>
+                    <div class="col-md card-grow rounded m-1 py-2 blue-background white-text clickable" onClick={() => window.location = "/reviews"}>
                         <h3 class="display-4">What Our Customers Have To Say</h3>
                         <p>
                         Find out what it's like to hire us.
                         </p>
                     </div>
-                    <div class="col-md card-grow rounded m-1 py-2 blue-background clickable" onClick={() => window.location = "/careers"}>
+                    <div class="col-md card-grow rounded m-1 py-2 blue-background white-text clickable" onClick={() => window.location = "/careers"}>
                         <h3 class="display-4">Interested in Working With Us?</h3>
                         <p>
                         Learn more about careers at Turner Concrete.
