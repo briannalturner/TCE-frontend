d72b008c8628987609cc2098cddb18c04300cf75
briannalturner <blturner3527@gmail.com>
2020-07-01 22:39:00 -0400

    working on careers carousel

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import career2 from '../pictures/career2.JPG'
+import career3 from '../pictures/career3.JPG'
+import career4 from '../pictures/career4.JPG'
+
 
 class Careers extends React.Component {
 
+    constructor() {
+        super()
+
+        this.state = {
+            picArray: ['../pictures/career2.JPG', {career3}, {career4}]
+        }
+    }
+
     render() {
         return (
-            <div>
-                Careers
+            <div className='float-middle'>
+                <div className='jumbotron jumbotron-fluid px-5 mb-2 careers-background'>
+                    <h1 className='display-2 text-center'>Careers at Turner Concrete & Excavating</h1>
+                </div>
+                <div className="row my-3 mx-4">
+                    <div className='col-4 text-left p-3'>
+                        <h3 className='m-1 text-center'><strong>Work With Us</strong></h3>
+                        <hr></hr>
+                        <div className="mb-3">Turner Concrete & Excavating has been recognized as a top employer by Fortune, The Washington Post, and Great Place to Work. We top the charts as an organization that focuses on team member well-being and the shared values and pride among our team. </div>
+                        <div className='mb-3'>We're always looking for friendly and highly motivated potential Inkers who are ready to roll up their sleeves and work with a great team.</div>
+                        <div className='mb-3'>If you think you’ve got what it takes to be an integral part of our internal community and succeed in a results-driven and fun environment, we’d love to hear from you.</div>
+                    </div>
+                    <div className='col-8 carousel'>
+                        <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-chevron-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                        </svg>
+                        <img className="career2 rounded float-center" src={this.state.picArray[0]} alt="" />
+                        <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                    </div>
+                </div>
             </div>
         )
     }
