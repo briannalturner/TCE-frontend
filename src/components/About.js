947c46c724081e241e00289af382ec2db5c72653
briannalturner <blturner3527@gmail.com>
2020-07-14 17:26:20 -0400

    added jumbotron to about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,12 @@ class About extends React.Component {
     render() {
         return (
             <div className="container">
-                <div class="about-section blue-background">
-                    <h1>About Us</h1>
-                    <p>Some text about who we are and what we do.</p>
-                    <p>Resize the browser window to see that this page is responsive by the way.</p>
+                <div className='jumbotron jumbotron-fluid px-5 mb-2 reviews-background'>
+                    <h1 className='display-2 text-center'>Family Owned And Operated Since 1997</h1>
+                    <p className='lead'>We've been serving the Shenandoah Valley for over 20 years and we're excited to help you take your private or professional project to the next level.</p>
                 </div>
 
-                <h2 style={{textAlign:'center'}}>Our Team</h2>
+                <h2 style={{textAlign:'center'}}>Meet Our Team</h2>
 
                     <div class="row" style={{width: '100%'}}>
                     <div class="col-3">
