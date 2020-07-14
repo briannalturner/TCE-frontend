2d4c2557647b73fa76f4aa2cf2b748067364e575
briannalturner <blturner3527@gmail.com>
2020-07-14 17:21:16 -0400

    fixed reviews grid

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import career2 from '../pictures/career2.JPG'
+import career4 from '../pictures/career4.JPG'
 
 class Reviews extends React.Component {
 
@@ -12,18 +13,20 @@ class Reviews extends React.Component {
                             <h1 className='display-2 text-center'>Read Reviews From Real Customers</h1>
                         </div>
                         <div>
-                            <div className="row">
-                                <div className="col">
-                                    <p class="triangle-border left">"I loved my experience working with Turner Concrete & Excavating, the entire process was seamless from start to finish. If you need a professional, dependable concrete company look no further." - Jane Doe, Shenandoah, VA</p>
+                            <div className="row mb-2">
+                                <div className="col-7">
+                                    <img style={{width: 'inherit'}} class='rounded' alt="" src={career4} />
                                 </div>
-
+                                <div className="col-5">
+                                    <p class="vertical-center triangle-border left">"I loved my experience working with Turner Concrete & Excavating, the entire process was seamless from start to finish. If you need a professional, dependable concrete company look no further." - Jane Doe, Shenandoah, VA</p>
+                                </div>
                             </div>
                             <div className="row">
-                                <div className="col-8">
-                                    <p class="triangle-border right">"We've depended on Turner Concrete & Excavating for years, and they have not yet let us down. Every time we need a project completed swiftly and professionally we call Turner Concrete." - Town of Luray, VA</p>
+                                <div className="col-5">
+                                    <p class=" vertical-center triangle-border right">"We've depended on Turner Concrete & Excavating for years, and they have not yet let us down. Every time we need a project completed swiftly and professionally we call Turner Concrete." - Town of Luray, VA</p>
                                 </div>
-                                <div className="col-4">
-                                    <img style={{width: 'inherit'}} src={career2} />
+                                <div className="col-7">
+                                    <img style={{width: 'inherit'}} class='rounded' alt="" src={career2} />
                                 </div>
                             </div>
                         </div>
