7466cf729ec038e39b07625e466a3b2353dbd94c
briannalturner <blturner3527@gmail.com>
2020-07-14 17:48:46 -0400

    fixed photo spacing about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,40 +8,41 @@ class About extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <div className='jumbotron jumbotron-fluid px-5 mb-2 reviews-background'>
+            <div className="">
+                <div className='jumbotron jumbotron-fluid px-5 mb-2 about-background'>
                     <h1 className='display-2 text-center'>Family Owned And Operated Since 1997</h1>
                     <p className='lead'>We've been serving the Shenandoah Valley for over 20 years and we're excited to help you take your private or professional project to the next level.</p>
                 </div>
-
-                <h2 style={{textAlign:'center'}}>Meet Our Team</h2>
+                <div className='m-1 p-1'>
+                    <h1 className=''>Meet Our Team</h1>
 
                     <div class="row" style={{width: '100%'}}>
-                    <div class="col-3">
-                        <div class="card float-left">
-                            <img src={Ronnie} alt="Mike" className="card-img-top" style={{height: '250px', objectFit: 'cover'}}/>
-                            <div class="card-body">
-                                <h2>Ronnie Turner</h2>
-                                <p class="title">Founder</p>
+                        <div class="col-4 float-left">
+                            <div class="card clickable">
+                                <img src={Ronnie} alt="Mike" className="card-img-top" style={{height: '250px', objectFit: 'cover'}}/>
+                                <div class="card-body">
+                                    <h2>Ronnie Turner</h2>
+                                    <p class="title">Founder</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-3 float-center">
-                        <div class="card">
-                            <img src={Jonny} alt="John" className="card-img-top" style={{height: '250px', objectFit: 'cover'}}/>
-                            <div class="card-body">
-                                <h2>Jon Turner</h2>
-                                <p class="title">CEO</p>
+                        <div class="col-4 float-center">
+                            <div class="card clickable">
+                                <img src={Jonny} alt="John" className="card-img-top" style={{height: '250px', objectFit: 'cover'}}/>
+                                <div class="card-body">
+                                    <h2>Jon Turner</h2>
+                                    <p class="title">CEO</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                        <div class="col-3 float-right">
-                            <div class="card">
-                            <img src={Carol} alt="Jane" className="card-img-top" style={{height: '250px', objectFit: 'cover'}}/>
-                            <div class="card-body">
-                                <h2>Carol Turner</h2>
-                                <p class="title">CFO</p>
+                            <div class="col-4 float-right">
+                                <div class="card clickable">
+                                <img src={Carol} alt="Jane" className="card-img-top" style={{height: '250px', objectFit: 'cover'}}/>
+                                <div class="card-body">
+                                    <h2>Carol Turner</h2>
+                                    <p class="title">CFO</p>
+                                </div>
                             </div>
                         </div>
                     </div>
