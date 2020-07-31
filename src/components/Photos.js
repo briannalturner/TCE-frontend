9fd12a9805c5a4654c72a41ddd048794479ac07c
briannalturner <blturner3527@gmail.com>
2020-07-31 18:50:45 -0400

    photos page layout finished

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import career2 from '../pictures/career2.JPG'
 
 class Photos extends React.Component {
 
     render() {
         return (
-            <div>
-                Photos
+            <div className='my-3'>
+                <h1 className='text-middle mt-4'>Photos</h1>
+                <div className='row'>
+                    <div className='col'>
+                        <img src={career2} className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={career2} className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={career2} className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                    </div>
+                    <div className='col'>
+                        <img src={career2} className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={career2} className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={career2} className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                    </div>
+                    <div className='col'>
+                        <img src={career2} className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={career2} className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={career2} className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                    </div>
+                </div>
             </div>
         )
     }
