d9b4f9622ff94131759d37669eeedbf2074d049c
briannalturner <blturner3527@gmail.com>
2020-08-03 15:32:55 -0400

    added new pics to lightbox and page"

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import career2 from '../pictures/career2.JPG'
+import brush_sidewalk from '../pictures/brush_sidewalk.JPG'
 import Lightbox from 'react-image-lightbox';
+import curved_sidewalk from '../pictures/curved_sidewalk.JPG'
+import parking from '../pictures/parking.JPG'
+import pool from '../pictures/pool.JPG'
+import porch from '../pictures/porch.JPG'
+import street from '../pictures/street.JPG'
+import walkway from '../pictures/walkway.JPG'
+import excavating from '../pictures/excavating.JPG'
 
 const images = [
-    'https://images.unsplash.com/photo-1560155069-ad79768f2666?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    'https://images.unsplash.com/photo-1587393855524-087f83d95bc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    'https://images.unsplash.com/photo-1518635017498-87f514b751ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    'https://images.unsplash.com/photo-1553549216-903af0603b16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-  ];
+    'http://localhost:3000/static/media/career2.c163616f.JPG',
+    'http://localhost:3000/static/media/brush_sidewalk.fe94a2b3.JPG',
+    'http://localhost:3000/static/media/curved_sidewalk.0af55566.JPG',
+    'http://localhost:3000/static/media/parking.536dbc86.JPG',
+    'http://localhost:3000/static/media/pool.f5c86884.JPG',
+    'http://localhost:3000/static/media/porch.dbaccbd2.JPG',
+    'http://localhost:3000/static/media/excavating.76922218.JPG',
+    'http://localhost:3000/static/media/walkway.7402992c.JPG',
+    'http://localhost:3000/static/media/street.2eac350b.JPG',
+];
 
 class Photos extends React.Component {
 
@@ -21,54 +34,25 @@ class Photos extends React.Component {
     }
 
     render() {
-
         const { photoIndex, isOpen } = this.state;
-
-        // return (
-        //     <div>
-        //       <button type="button" onClick={() => this.setState({ isOpen: true })}>
-        //         Open Lightbox
-        //       </button>
-       
-        //       {isOpen && (
-        //         <Lightbox
-        //           mainSrc={images[photoIndex]}
-        //           nextSrc={images[(photoIndex + 1) % images.length]}
-        //           prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-        //           onCloseRequest={() => this.setState({ isOpen: false })}
-        //           onMovePrevRequest={() =>
-        //             this.setState({
-        //               photoIndex: (photoIndex + images.length - 1) % images.length,
-        //             })
-        //           }
-        //           onMoveNextRequest={() =>
-        //             this.setState({
-        //               photoIndex: (photoIndex + 1) % images.length,
-        //             })
-        //           }
-        //         />
-        //       )}
-        //     </div>
-        //   );
-
         return (
             <div className='my-3'>
                 <h1 className='text-middle mt-4'>Photos</h1>
                 <div className='row'>
                     <div className='col'>
                         <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:0 })} alt='' className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:3 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:6 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={parking} onClick={() => this.setState({ isOpen: true, photoIndex:3 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={excavating} onClick={() => this.setState({ isOpen: true, photoIndex:6 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
                     </div>
                     <div className='col'>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:1 })} alt='' className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:4 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:7 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={brush_sidewalk} onClick={() => this.setState({ isOpen: true, photoIndex:1 })} alt='' className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={porch} onClick={() => this.setState({ isOpen: true, photoIndex:4 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={walkway} onClick={() => this.setState({ isOpen: true, photoIndex:7 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
                     </div>
                     <div className='col'>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:2 })} alt='' className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:5 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
-                        <img src={career2} onClick={() => this.setState({ isOpen: true, photoIndex:8 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={curved_sidewalk} onClick={() => this.setState({ isOpen: true, photoIndex:2 })} alt='' className='border mb-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={pool} onClick={() => this.setState({ isOpen: true, photoIndex:5 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
+                        <img src={street} onClick={() => this.setState({ isOpen: true, photoIndex:8 })} alt='' className='border my-3 rounded clickable' style={{maxHeight: 'inherit', width:'inherit', objectFit:'cover'}}/>
                     </div>
                 </div>
                 {isOpen && (
